5fc96e1c1ee72355029f10e427b2ce221f93fa37
Benill0 <elbenitope@gmail.com>
2025-03-29 18:09:25 -0500

    feat(arreglos-florales): :sparkles: add flower arrangement page with styles and content display

---
+++ src/app/arreglos-florales/[id]/page.tsx
@@ -0,0 +1,59 @@
+import { getFlowerArrangementById } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+
+interface Params {
+  id: string;
+}
+
+export default async function FlowerArrangementPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { id: flowerId } = await params;
+
+  if (!flowerId) {
+    return notFound();
+  }
+  const flower = getFlowerArrangementById(parseInt(flowerId));
+
+  if (!flower) {
+    return notFound();
+  }
+
+  return (
+    <div className={styles.page}>
+      <h1>
+        {flower.name} - {flower.id}
+      </h1>
+      <Image src={flower.url} alt={flower.name} width={500} height={500} />
+      <div className={styles.description}>
+        <div className={styles.content}>
+          <h2>Contenido</h2>
+          <ul className={styles.listContent}>
+            {flower.content.map((item, index) => (
+              <li key={index}>- {item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.wrapperBuy}>
+          <button>COMPRAR</button>
+          <div className={styles.wrapperPayment}>
+            <label className={styles.labelPayment}>Método de pago</label>
+            <Link href="https://www.yape.com.co/">
+              <Image
+                src="/images/yape.webp"
+                alt="yape"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
