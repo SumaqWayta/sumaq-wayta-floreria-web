859fceefe05643389a87c876a380e7315eb94424
Benill0 <elbenitope@gmail.com>
2025-04-02 00:58:13 -0500

    feat(arreglos-funebres): :sparkles: add page and styles for individual funeral flower arrangements with image slider

---
+++ src/app/arreglos-funebres/[type]/[id]/page.tsx
@@ -0,0 +1,65 @@
+import SliderImage from "@/components/slider-images/slider-image";
+import { getFuneralFlowerByName, getRandomFuneralFlowers } from "@/lib/data";
+import { FuneralFlowerTypes } from "@/types/flower";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+
+interface Params {
+  id: string;
+  type: FuneralFlowerTypes;
+}
+
+export default async function FlowerArrangementPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { id: flowerId, type: flowerType } = await params;
+  if (!flowerId || !flowerType) {
+    return notFound();
+  }
+  const flower = getFuneralFlowerByName(flowerType, parseInt(flowerId));
+  const randomFlowers = getRandomFuneralFlowers(flowerType, parseInt(flowerId));
+
+  if (!flower || !randomFlowers) {
+    return notFound();
+  }
+
+  return (
+    <div className={styles.page}>
+      <h1>
+        {flower.name} - {flower.id}
+      </h1>
+      <Image src={flower.url} alt={flower.name} width={500} height={500} />
+      <div className={styles.description}>
+        <div className={styles.content}>
+          <h2>Contenido</h2>
+          <ul className={styles.listContent}>
+            {flower.content.map((item, index) => (
+              <li key={index}>- {item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.wrapperBuy}>
+          <button>COMPRAR</button>
+          <div className={styles.wrapperPayment}>
+            <label className={styles.labelPayment}>Método de pago</label>
+            <Link href="https://www.yape.com.co/">
+              <Image
+                src="/images/yape.webp"
+                alt="yape"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wrapperSlider}>
+        <SliderImage data={randomFlowers} />
+      </div>
+    </div>
+  );
+}
