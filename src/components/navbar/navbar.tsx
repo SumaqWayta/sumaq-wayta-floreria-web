bc42e7af60e52af9367850a7f073ffb8bb019489
Benill0 <elbenitope@gmail.com>
2025-04-27 19:06:11 -0500

    feat(navbar): :sparkles: add ShoppingCartSVG and improve sidebar handling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ShoppingCartSVG from "@/assets/icons/shopping-cart";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -18,10 +19,16 @@ export const Navbar = () => {
     setShowSidebar(!showSidebar);
   };
 
+  const closeSidebar = () => {
+    setShowSidebar(false);
+    document.body.style.overflow = "auto";
+  };
+
   return (
     <>
       <header className={styles.wrapperNavbar}>
         <div className={styles.container}>
+          <ButtonHamburger value={showSidebar} onClick={changeShowSidebar} />
           <Link href="/">
             <Image src="/svg/logo.svg" alt="Logo" width={100} height={70.7} />
           </Link>
@@ -31,7 +38,9 @@ export const Navbar = () => {
             <Link href="/arreglos-florales">Arreglos Florales</Link>
             <Link href="/ramos">Ramos</Link>
           </nav>
-          <ButtonHamburger value={showSidebar} onClick={changeShowSidebar} />
+          <Link href="/carro" onClick={closeSidebar}>
+            <ShoppingCartSVG fill="var(--main-color)" width={36} height={36} />
+          </Link>
         </div>
       </header>
       <Sidebar value={showSidebar} onClose={changeShowSidebar} />
