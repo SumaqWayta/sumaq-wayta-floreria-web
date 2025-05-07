aef6061a456507d9d6323e5f86cfa39df9ae9a34
Benill0 <elbenitope@gmail.com>
2025-05-07 13:46:41 -0500

    feat: update flowers database

---
+++ src/database/mocks.ts
@@ -0,0 +1,73 @@
+/**
+ * ARRANGEMENTS
+ * 
+ *   {
+    "id": 2,
+    "name": "Arreglo Floral",
+    "url": "/flowers/flower-arrangements/arrangement-2.svg",
+    "link": "/arreglos-florales/2",
+    "content": [
+      "Rosas",
+      "Gerberas",
+      "Astromelias",
+      "Claveles",
+      "Imperico",
+      "Follaje fino"
+    ],
+    "price": "69.90"
+  },
+  {
+    "id": 3,
+    "name": "Arreglo Floral",
+    "url": "/flowers/flower-arrangements/arrangement-3.svg",
+    "link": "/arreglos-florales/3",
+    "content": [
+      "Girasoles",
+      "Rosas",
+      "Imperico",
+      "Astromelias",
+      "Follaje fino"
+    ],
+    "price": "69.90"
+  },
+  {
+    "id": 4,
+    "name": "Arreglo Floral",
+    "url": "/flowers/flower-arrangements/arrangement-4.svg",
+    "link": "/arreglos-florales/4",
+    "content": [
+      "Rosas",
+      "Girasoles",
+      "Claveles",
+      "Verónica",
+      "Astromelias",
+      "Follaje fino"
+    ],
+    "price": "79.90"
+  },
+ * 
+  {
+    "id": 6,
+    "name": "Arreglo Floral",
+    "url": "/flowers/flower-arrangements/arrangement-6.svg",
+    "link": "/arreglos-florales/6",
+    "content": [
+      "Rosas",
+      "Gerbera",
+      "Astromelias",
+      "Claveles",
+      "Verónica",
+      "Follaje fino"
+    ],
+    "price": "59.90"
+  },
+  
+  {
+    "id": 8,
+    "name": "Arreglo Floral",
+    "url": "/flowers/flower-arrangements/arrangement-8.svg",
+    "link": "/arreglos-florales/8",
+    "content": ["Rosas", "Astromelias", "Verónica", "Follaje fino"],
+    "price": "79.90"
+  },
+ */
