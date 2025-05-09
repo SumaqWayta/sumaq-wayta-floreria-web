a0195a8f5b7a046d77965d7a76ed55cdd885fb76
Benill0 <elbenitope@gmail.com>
2025-05-09 01:50:03 -0500

    feat(payment-method): add extra information in options payment method

--- src/app/carro/_components/payment-method/payment-method.tsx
+++ src/app/carro/_components/payment-method/payment-method.tsx
@@ -38,40 +38,50 @@ export default function PaymentMethod({
       <h2 className={styles.title}>Metodo de pago</h2>
       <p>Seleccione el metodo de pago</p>
       <div className={styles.options}>
-        <div className={styles.option}>
-          <input
-            type="radio"
-            id="yape"
-            name="paymentMethod"
-            onChange={handleChangePaymentMethod}
-            value={"yape"}
-            checked={paymentMethod === "yape"}
-          />
-          <label htmlFor="yape">Yape</label>
+        <div className={styles.wrapperOption}>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="yape"
+              name="paymentMethod"
+              onChange={handleChangePaymentMethod}
+              value={"yape"}
+              checked={paymentMethod === "yape"}
+            />
+            <label htmlFor="yape">Yape</label>
+          </div>
+          <div className={styles.transferData}>
+            <p>
+              <span>Numero:</span> +51 992776777
+            </p>
+            <p>
+              <span>Beneficiario:</span> Jesus Villano Buendia
+            </p>
+          </div>
         </div>
-        <div className={styles.option}>
-          <input
-            type="radio"
-            id="transferencia"
-            name="paymentMethod"
-            value={"transferencia"}
-            onChange={handleChangePaymentMethod}
-            checked={paymentMethod === "transferencia"}
-          />
-          <label htmlFor="transferencia">Transferencia Bancaria</label>
-        </div>
-        <div className={styles.option}>
-          <input
-            type="radio"
-            id="transferencia-interbancaria"
-            name="paymentMethod"
-            value={"transferencia-interbancaria"}
-            onChange={handleChangePaymentMethod}
-            checked={paymentMethod === "transferencia-interbancaria"}
-          />
-          <label htmlFor="transferencia-interbancaria">
-            Transferencia Interbancaria
-          </label>
+        <div className={styles.wrapperOption}>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="transferencia"
+              name="paymentMethod"
+              value={"transferencia"}
+              onChange={handleChangePaymentMethod}
+              checked={paymentMethod === "transferencia"}
+            />
+            <label htmlFor="transferencia">Transferencia BCP</label>
+          </div>
+          <div className={styles.transferData}>
+            <p>
+              <span>Cuenta:</span> 19478023503018
+            </p>
+            <p>
+              <span>CCI:</span> 00219417802350301892
+            </p>
+            <p>
+              <span>Beneficiario:</span> Jesus Villano Buendia
+            </p>
+          </div>
         </div>
       </div>
       <div className={styles.buttons}>
