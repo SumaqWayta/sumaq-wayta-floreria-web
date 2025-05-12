a093b7d4345482692074100adf9944ec02532a9d
Benill0 <elbenitope@gmail.com>
2025-05-11 21:07:36 -0500

    feat(routes): add routes in website

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = [
+  "/",
+  "/arreglos-funebres",
+  "/arreglos-funebres?tipo=1",
+  "/arreglos-funebres?tipo=2",
+  "/arreglos-funebres?tipo=3",
+  "/arreglos-funebres?tipo=4",
+  "/arreglos-florales",
+  "/ramos",
+  "/carro",
+];
